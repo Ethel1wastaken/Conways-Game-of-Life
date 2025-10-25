0703cfc539cbecdf36d6b745c293a97a50499085
Ethel1wastaken <b2029egtgamis@pshs.edu.ph>
2025-10-25 21:29:57 +0800

    replaced generateGrid with renderGrid, which renders alive cells based on a matrix

--- public/conways_playground.js
+++ public/conways_playground.js
@@ -1,16 +1,37 @@
-function generateGrid(rows, cols) {
+function renderGrid(state) {
     const grid = document.getElementById("grid");
     grid.innerHTML = "";
+
+    const rows = state.length;
+    const cols = state[0].length;
 
     for(let r=0; r<rows; r++) {
         const row = document.createElement("tr");
         grid.appendChild(row);
+
         for(let c=0; c<cols; c++) {
             const cell = document.createElement("td");
             cell.classList.add("cell");
-            row.appendChild(cell);  
+            row.appendChild(cell);
+
+            if (state[r][c] === 1) {
+                cell.classList.add("alive");
+            }
         }
     }
 }
 
-generateGrid(10, 10);
+let state = [
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+renderGrid(state);
